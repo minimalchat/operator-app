f3e891c4875b7b427c262f7acf7393701d4e48c8
teesloane <tylersloane@gmail.com>
2017-03-13 12:55:44 -0400

    Add redux logger
    - this commit implements redux logger, which I think will make the dev
    environment a bit easier for debugging (since we can't have redux dev tools)
    
    - redux logger is a middleware (it interceptions actions before they reach a
      reducer, and do...something with that data)
    
    - here's the [repo](https://github.com/evgenyrodionov/redux-logger#transform-immutable-without-combinereducers) for more information
    - I also simplified the rendering of the react app - I don't think React.Dom
    needs to be wrapped in a function expression and stored in a variable that's
    - called. Just keeps it a bit cleaner with less variables names in the mix.
    
    😶😶😶 :)

--- src/init.jsx
+++ src/init.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
 
 import Application from './components/Application/Application.jsx';
 
 // Reducers
 import chat from './store/Chat/reducer';
 
+
+// middleware
+const logger = createLogger();        // TODO: make DEV only.
+
+
+// Create redux store
 const store = createStore(
   combineReducers({
     chat,
   }),
+
+  applyMiddleware(logger)             // NOTE: `logger` must come last
 );
 
-// just for debugging
+
+// Debugging Tooling
+store.subscribe(() => console.log('DEBUG', store.getState()));
 window.store = store.getState();
 
-const render = function Render () {
-  ReactDOM.render(
-    <Provider store={store}>
-      <Application />
-    </Provider>,
-    document.getElementById('app'),
-  );
-};
 
-store.subscribe(() => {
-  console.log('DEBUG', store.getState());
-});
-
-
-// Start rendering
-render();
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>,
+  document.getElementById('app'),
+);
