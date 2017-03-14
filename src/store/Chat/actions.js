aedb0322fe275280efd0ccc06b0faa1f92b151c2
teesloane <tylersloane@gmail.com>
2017-03-14 17:50:02 -0400

    Remove redundant action

--- src/store/Chat/actions.js
+++ src/store/Chat/actions.js
@@ -4,13 +4,6 @@ import {
 } from './constants';
 
 
-export function openConversation (payload) {
-  return {
-    type: CHAT_OPEN_CONVERSATION,
-    payload,
-  };
-}
-
 export function tagClient (payload) {
   return {
     type: CHAT_TAG_CLIENT,
