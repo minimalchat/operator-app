fe18479ec4e2aee54148575700328d4d81852815
teesloane <tylersloane@gmail.com>
2017-03-14 22:44:00 -0400

    Create and display <Message> component.
    Also updated `dummy` function to make some messages come from
    an operator on a random basis.

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,24 @@
+/**
+  * @summary: Displays a single message in the Message List
+*/
+
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import './Message.css';
+
+const Message = props => {
+  const msgClass = () => (
+    props.type === 'operator' ? 'Message__operator' : 'Message__client'
+  );
+
+  return <div className={msgClass()}>dklfj{props.children}</div>
+};
+
+
+
+export default Message
+
+
+Message.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- src/components/MessageList/MessageList.jsx
+++ src/components/MessageList/MessageList.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import './MessageList.css';
+import Message from '../Message/Message.jsx';
 
 /**
  * @summary: responsible for handling the display of messages.
@@ -8,16 +9,15 @@ import './MessageList.css';
 
 const MessageList = (props) => {
   const { messages, active } = props;
-
-  const activeMsgs = messages.filter(msg => (
-     msg.chat === active
-  ));
+  const activeMsgs = messages.filter(msg => msg.chat === active);
 
 
+  // render a map of <Message> components with their contents.
   const renderView = () => {
     if (!active) return (<div className="MessageList__empty"> No chat selected </div>);
-    return activeMsgs.map(msg => (<div className="ML-m-client">{msg.content}</div>));
+    return activeMsgs.map(msg => (<Message type={msg.author}>{msg.content}</Message>));
   };
+
 
   return (
     <div className="MessageList">
