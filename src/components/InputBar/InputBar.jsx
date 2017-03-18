1a6bd94b1330706d96d5470b7833640d2bb56fbd
teesloane <tylersloane@gmail.com>
2017-03-17 20:55:48 -0400

    Make chat text box into class based components for testing sockets
    - no point in hooking up the text chat thing to redux yet until we actually have
      sockets working.

--- src/components/InputBar/InputBar.jsx
+++ src/components/InputBar/InputBar.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from '../Button/Button.jsx';
 import './InputBar.css';
 
-const InputBar = () => (
-  <div className="InputBar">
-    <textarea placeholder="text here, dummy" />
-    <Button variant="send" onClick={() => {}}>Send</Button>
-  </div>
-);
+class InputBar extends Component {
 
+  state = {
+    chatText: '',
+  }
+
+  sendChat = () => {
+    console.log('aldkfjalkd');
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  render () {
+    return (
+      <div className="InputBar">
+        <textarea onChange={this.handleChange} name="chatText" placeholder="text here, dummy" />
+        <Button variant="send" onClick={() => {}}>Send</Button>
+      </div>
+    );
+  }
+}
+
+/* const InputBar = () => (
+ *   <div className="InputBar">
+ *     <textarea placeholder="text here, dummy" />
+ *     <Button variant="send" onClick={() => {}}>Send</Button>
+ *   </div>
+ * );
+ * */
 export default InputBar;
