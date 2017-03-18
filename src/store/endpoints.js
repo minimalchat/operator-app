12ffe2d6e17c3c9090e700d1ec8389aeb869eca5
Bronek Szulc <bronek@fastmail.fm>
2017-03-18 16:39:45 -0400

    Chats are now loaded from daemon api. Disabled front end dummy data.

---
+++ src/store/endpoints.js
@@ -0,0 +1,12 @@
+const ENDPOINTS = {};
+
+ENDPOINTS.operators = '/api/operators';
+ENDPOINTS.operator = '/api/operator';
+
+ENDPOINTS.clients = '/api/clients';
+ENDPOINTS.client = '/api/client';
+
+ENDPOINTS.chats = '/api/chats';
+ENDPOINTS.chat = '/api/chat';
+
+export default ENDPOINTS;
