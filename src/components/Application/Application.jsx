6893b9dc3178f432048e327e3729f978539954bd
teesloane <tylersloane@gmail.com>
2017-03-17 20:10:57 -0400

    Add sockets to operator.
    Still need to hook up to the chat window...

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -1,13 +1,24 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import io from 'socket.io-client';
 
 import OperatorPanel from '../OperatorPanel/OperatorPanel.jsx';
 import ClientsPanel from '../ClientsPanel/ClientsPanel.jsx';
 import MessagePanel from '../MessagePanel/MessagePanel.jsx';
 import Settings from '../Settings/Settings.jsx';
+import initSockets from './Sockets.js';
 import './Application.css';
 
+const socketPath = 'http://localhost:8000';
+
 const Application = (props) => {
+  const socket = io.connect(socketPath, {
+    reconnectionAttempts: 10,
+  });
+
+  initSockets(socket);
+
+
   const renderMainView = () => (
     props.settingsOpen ? <Settings /> :
 
