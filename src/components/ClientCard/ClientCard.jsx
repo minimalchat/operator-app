4e86a48d5094b45279cdbf3d0b0fc00684deb442
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2017-08-27 21:57:45 -0400

    Adding initial jsx for client list, adding symbolset air icons

--- src/components/ClientCard/ClientCard.jsx
+++ src/components/ClientCard/ClientCard.jsx
@@ -4,35 +4,54 @@
   * TODO: style based on currently selected card.
 */
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { setActiveChat, loadMessages } from '../../store/Chat';
 import './ClientCard.css';
 
-const ClientCard = props => (
-  <li className="ClientCard" >
-    <button
-      className="ClientCard__btn"
-      onClick={() => {
-        props.setActiveChat(props.chat);
+class ClientCard extends Component {
+  static propTypes = {
+    chat: PropTypes.object.isRequired,
+    config: PropTypes.object.isRequired,
+    children: PropTypes.node.isRequired,
+    setActiveChat: PropTypes.func.isRequired,
+    loadMessages: PropTypes.func.isRequired,
+  }
 
-        props.loadMessages(props.config, props.chat.id);
-      }}
-    >
-      {props.children}
-    </button>
-  </li>
-);
+  select () {
+    const { chat, config } = this.props;
 
-ClientCard.propTypes = {
-  chat: PropTypes.object.isRequired,
-  config: PropTypes.object.isRequired,
-  children: PropTypes.node.isRequired,
-  setActiveChat: PropTypes.func.isRequired,
-  loadMessages: PropTypes.func.isRequired,
-};
+    this.props.setActiveChat(chat);
 
+    this.props.loadMessages(config, chat.id);
+  }
+
+  render () {
+    return (
+      <li className="ClientCard">
+        <button
+          onClick={this.select}
+          className="ClientCard__btn"
+        >
+          <div className="ClientCard__icon ss-icon">
+            <span>user</span>
+          </div>
+          <div className="ClientCard__information">
+            <div className="ClientCard__information-row">
+              <span className="ClientCard__name">{this.props.children}</span>
+              <span className="ClientCard__status" />
+            </div>
+            <div className="ClientCard__information-row">
+              <p className="ClientCard__lastmsg">Lorem ipsum dolor sit amet</p>
+              <span className="ClientCard__lastmsgtime">2 minutes ago</span>
+            </div>
+          </div>
+        </button>
+      </li>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   config: state.chat.config,
