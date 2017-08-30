d7dd06c8b5c3ab26a28fe23f6a6d93132e7100b5
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2017-08-30 09:05:47 -0400

    Adding additional check for only client messages and fixing default window size

--- Window.js
+++ Window.js
@@ -14,7 +14,7 @@ const buildMenu = require('./server/build-menu');
 const events = require('./server/events');
 
 const WINDOW_HEIGHT = 900;
-const WINDOW_WIDTH = 1440;
+const WINDOW_WIDTH = 1200;
 
 const defaultURL = url.format({
   pathname: path.join(__dirname, 'index.html'),
