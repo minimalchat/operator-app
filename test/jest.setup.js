88f64b5d5997068aed36e3d8907dd377ab4d5691
Matthew Mihok <4d.69.68.6f.6b@gmail.com>
2018-01-05 09:51:14 -0500

    Upgrading jest, jest-cli to 22.0.4, jest-enzyme to 4.0.2, enzyme, enzme-to-json to 3.3.0, fixing security vulnerability flagged by github

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,4 +1,8 @@
 // Setup anything for tests (but know that this is skipped by eslinting)
+var enzyme = require('enzyme');
+var Adapter = require('enzyme-adapter-react-15');
+
+enzyme.configure({ adapter: new Adapter() });
 
 // Skip createElement warnings but fail tests on any other warning
 console.error = message => {
